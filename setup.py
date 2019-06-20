6b057f40b4956fad5f90c5ad7341961236916835
andy920262 <920262@gmail.com>
2019-06-20 16:05:14 +0800

    Add setup.py
    * Can use `python3 setup.py install` to install the library now.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(name='miuds',
+      version='0.1',
+      description='Dialogue System',
+      packages=find_packages(),
+     )
